0df4487e1cbb7dd6212d7eb11d735d1063a99ecc
Redi Ahmad Supriyatna <ahmadredi67@gmail.com>
2024-12-08 00:10:00 +0700

    project init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int counter = 0;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  Serial.print(counter);
+  Serial.println("Hello world");
+  delay(2000);
+  counter++;
+}
