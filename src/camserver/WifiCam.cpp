599d386dda49a18f39c9c9e7f31e4fa63350bfa6
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-20 01:00:38 +0700

    feat: stream mjpeg w/ protocol

---
+++ src/camserver/WifiCam.cpp
@@ -0,0 +1,43 @@
+#include "WifiCam.hpp"
+#include <WiFi.h>
+
+using namespace esp32cam;
+
+WebServer server(80);
+
+void initCam(int minWidth, int minHeight, int quality)
+{
+	Resolution initialResolution = Resolution::find(minWidth, minHeight);
+
+	Config config;
+	config.setPins(pins::AiThinker);
+	config.setResolution(initialResolution);
+	config.setJpeg(quality);
+
+	bool isCamBegin = Camera.begin(config);
+	if (!isCamBegin)
+	{
+		Serial.println("[FAILED] Camera cannot be started :(");
+		Serial.println("[INFO] Restarting board in 3s...");
+		delay(3000);
+		ESP.restart();
+	}
+	Serial.println("[SUCCESS] Camera initialized. Hurrayy :)");
+}
+
+void beginServer()
+{
+	server.begin();
+}
+
+void handleClient()
+{
+	server.handleClient();
+}
+
+void showStreamURL()
+{
+	IPAddress ipv4 = WiFi.localIP();
+	char url[64];
+	snprintf(url, sizeof(url), "http://%d.%d.%d.%d/640x480.mjpeg", ipv4[0], ipv4[1], ipv4[2], ipv4[3]);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,22 @@
 #include "iot/iot.h"
 #include "camserver/WifiCam.hpp"
 
-esp32cam::Resolution initialResolution;
-WebServer server(80);
-
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
   initWiFi();
   initBlynk();
+  initCam();
+
+  addRequestHandlers();
+  beginServer();
+  showStreamURL();
 }
 
 void loop()
 {
   runBlynk();
+  handleClient();
 }
