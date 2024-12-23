2abd84f0bf0b85e6f6a3977d96a131625dc74a7f
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-23 12:11:40 +0700

    chore: added context in credentials

--- src/iot/credentials.example.h
+++ src/iot/credentials.example.h
@@ -1,11 +1,11 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "PASTE YOUR TEMPLATE ID"
-#define BLYNK_TEMPLATE_NAME "PASTE YOUR TEMPLATE NAME"
-#define BLYNK_AUTH_TOKEN "PASTE YOUR BLYNK AUTHENTICATION TOKEN"
-#define BLYNK_CUSTOM_HOST_NAME "your.custom.hostname"
-#define BLYNK_CUSTOM_PORT "YOUR PORT NUMBER"
+#define BLYNK_TEMPLATE_ID "PASTE YOUR TEMPLATE ID" // required
+#define BLYNK_TEMPLATE_NAME "PASTE YOUR TEMPLATE NAME" // required
+#define BLYNK_AUTH_TOKEN "PASTE YOUR BLYNK AUTHENTICATION TOKEN" // required
+#define BLYNK_CUSTOM_HOST_NAME "your.custom.hostname" // required
+#define BLYNK_CUSTOM_PORT "YOUR PORT NUMBER" // required
 
-#define MANUAL_SSID "SSID Name"
-#define MANUAL_PASSWORD "wifi password"
-#define ACCESS_POINT_NAME "AP Name"
-#define ACCESS_POINT_PASSWORD "AP Password"
+#define MANUAL_SSID "SSID Name" // optional, if you want to connect to a specific WiFi
+#define MANUAL_PASSWORD "wifi password" // optional, if you want to connect to a specific WiFi
+#define ACCESS_POINT_NAME "AP Name" // required
+#define ACCESS_POINT_PASSWORD "AP Password" // optional, if you want to create a protected access point
