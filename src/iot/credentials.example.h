f4125ecd545a589d7c549e721218f961379e65c1
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-24 17:54:42 +0700

    docs: added descriptive credentials guide

--- src/iot/credentials.example.h
+++ src/iot/credentials.example.h
@@ -1,9 +1,43 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_AUTH_TOKEN "PASTE YOUR BLYNK AUTHENTICATION TOKEN" // required
-#define BLYNK_CUSTOM_HOST_NAME "your.custom.hostname" // required
-#define BLYNK_CUSTOM_PORT "YOUR PORT NUMBER" // required
 
-#define MANUAL_SSID "SSID Name" // optional, if you want to connect to a specific WiFi
-#define MANUAL_PASSWORD "wifi password" // optional, if you want to connect to a specific WiFi
-#define ACCESS_POINT_NAME "AP Name" // required. This is the name of the access point that will be created if the device cannot connect to the specified WiFi
-#define ACCESS_POINT_PASSWORD "AP Password" // optional, if you want to create a protected access point
+/*
+ * BLYNK_AUTH_TOKEN
+ * Blynk authentication token (required)
+ */
+#define BLYNK_AUTH_TOKEN "PASTE YOUR BLYNK AUTHENTICATION TOKEN"
+
+/*
+ * BLYNK_CUSTOM_HOST_NAME
+ * Custom Blynk server hostname (required)
+ */
+#define BLYNK_CUSTOM_HOST_NAME "your.custom.hostname"
+
+/*
+ * BLYNK_CUSTOM_PORT
+ * Custom Blynk server port number (required)
+ */
+#define BLYNK_CUSTOM_PORT 8080
+
+/*
+ * MANUAL_SSID
+ * SSID for manual WiFi connection (optional, use with initWiFiManually())
+ */
+#define MANUAL_SSID "SSID Name"
+
+/*
+ * MANUAL_PASSWORD
+ * Password for manual WiFi connection (optional, use with initWiFiManually())
+ */
+#define MANUAL_PASSWORD "wifi password"
+
+/*
+ * ACCESS_POINT_NAME
+ * Name of the access point to be created if WiFi connection fails (required)
+ */
+#define ACCESS_POINT_NAME "AP Name"
+
+/*
+ * ACCESS_POINT_PASSWORD
+ * Password for the access point to be created if WiFi connection fails (optional)
+ */
+#define ACCESS_POINT_PASSWORD "AP Password"
