7b33c7323b8055c67ce35b7868b951d43286a7a1
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-24 17:43:34 +0700

    chore: removed id and name for template

--- src/iot/credentials.example.h
+++ src/iot/credentials.example.h
@@ -1,11 +1,9 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "PASTE YOUR TEMPLATE ID" // required
-#define BLYNK_TEMPLATE_NAME "PASTE YOUR TEMPLATE NAME" // required
 #define BLYNK_AUTH_TOKEN "PASTE YOUR BLYNK AUTHENTICATION TOKEN" // required
 #define BLYNK_CUSTOM_HOST_NAME "your.custom.hostname" // required
 #define BLYNK_CUSTOM_PORT "YOUR PORT NUMBER" // required
 
 #define MANUAL_SSID "SSID Name" // optional, if you want to connect to a specific WiFi
 #define MANUAL_PASSWORD "wifi password" // optional, if you want to connect to a specific WiFi
-#define ACCESS_POINT_NAME "AP Name" // required
+#define ACCESS_POINT_NAME "AP Name" // required. This is the name of the access point that will be created if the device cannot connect to the specified WiFi
 #define ACCESS_POINT_PASSWORD "AP Password" // optional, if you want to create a protected access point
