8130368cb93898b4780febdcfcb833348baf3ef3
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-26 15:19:41 +0700

    docs: new credentials file naming convention

---
+++ src/ota/example.credentials.h
@@ -0,0 +1,15 @@
+/*
+ * OTA Credentials
+ *
+ * These credentials are used for Over-The-Air (OTA) updates.
+ * The username and password defined here will be required to authenticate
+ * when performing OTA updates to ensure that only authorized users can
+ * upload new firmware to the device.
+ *
+ * Make sure to replace the default values with your own secure credentials
+ * before deploying your device. Keep these credentials confidential to
+ * prevent unauthorized access.
+ */
+
+#define OTA_USERNAME "your_username" // Replace with your desired OTA username
+#define OTA_PASSWORD "your_password" // Replace with your desired OTA password
