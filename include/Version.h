6c968f325897d0a5ce84e5289c44d63f613864ee
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-28 01:39:32 +0700

    chore(versioning): new version

---
+++ include/Version.h
@@ -0,0 +1,9 @@
+
+    // AUTO GENERATED FILE, DO NOT EDIT
+    #ifndef VERSION
+        #define VERSION "3.0.1"
+    #endif
+    #ifndef BUILD_TIMESTAMP
+        #define BUILD_TIMESTAMP "2024-12-28 01:28:40.274033"
+    #endif
+    
