7041b2465c41205d831a0457cdf7571c97695dc1
rediahmads_ubuntu <redi.ngoding@gmail.com>
2024-12-30 21:41:04 +0700

    chore: new version build

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "3.0.3"
+        #define VERSION "3.0.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-12-28 21:33:40.576237"
+        #define BUILD_TIMESTAMP "2024-12-30 18:31:37.604578"
     #endif
     
