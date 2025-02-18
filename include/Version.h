87b67a0787faeabd3991f38b396635990709e9d7
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-02-18 12:15:43 +0700

    chore: :hammer: new build version

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "3.0.4"
+        #define VERSION "3.0.15"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-12-30 18:31:37.604578"
+        #define BUILD_TIMESTAMP "2025-02-18 12:00:32.907917"
     #endif
     
